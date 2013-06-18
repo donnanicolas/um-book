18815cc324c3ce577d4ac852f04ab02e311b6d5d
Juan Pedro Mermoz <jpmermoz@hotmail.com>
2013-06-18 10:56:02 -0300

    Fix foreign_key en post model

--- 4-Implementacion/app/models/post.rb
+++ 4-Implementacion/app/models/post.rb
@@ -2,8 +2,8 @@ class Post < ActiveRecord::Base
   attr_accessible :content, :user_a_id, :user_b_id
   validates_presence_of :content, :user_a_id, :user_b_id
  
-  belongs_to :user_sender, class_name: :User
-  belongs_to :user_receiver, class_name: :User
+  belongs_to :user_sender, foreign_key: :user_a_id, class_name: :User
+  belongs_to :user_receiver, foreign_key: :user_b_id, class_name: :User
 
   default_scope order('created_at DESC')
 end
