6cba8fbdba75011fafe9210322bc4cd2f58a9b3c
Juan Pedro Mermoz <jpmermoz@hotmail.com>
2013-06-18 10:17:26 -0300

    Validaciones en modelos

--- 4-Implementacion/app/models/post.rb
+++ 4-Implementacion/app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
   attr_accessible :content, :user_a_id, :user_b_id
-
+  validates_presence_of :content, :user_a_id, :user_b_id
+ 
   belongs_to :user_sender, class_name: :User
   belongs_to :user_receiver, class_name: :User
   
